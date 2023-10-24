3e3385bf137338a08a6cff19d3fe250c0b5fcc0d
mengtao.ye <mengtao.ye@shengydt.com>
2023-10-24 10:22:29 +0800

    新增AI寻路算法

---
+++ YFramework/Core/AI/Path/AIPath.cs
@@ -0,0 +1,19 @@
+﻿namespace YFramework
+{
+    public class AIPath
+    {
+        private NodeManager mNodeManager;
+        public NodeManager nodeManager => mNodeManager;
+        private CalcPathManager mCalcPathManager;
+        public CalcPathManager calcPathManager => mCalcPathManager;
+        public void Init()
+        {
+            mNodeManager = new NodeManager(this);
+            mCalcPathManager = new CalcPathManager(this);
+            mNodeManager.Awake();
+            mCalcPathManager.Awake();
+            mNodeManager.Start();
+            mCalcPathManager.Start();
+        }
+    }
+}
